f568afe7bfc94657e25c40c9b6235d765b394c7e
Sammy Ageil <sammy.ageil@outlook.com>
2024-08-20 22:53:30 -0400

    chore: improve neovim build

--- buildneovim.sh
+++ buildneovim.sh
@@ -5,7 +5,7 @@ build_neovim() {
   git clone https://github.com/neovim/neovim.git /nvimbuild/
   #cd .. && mkdir build && cd build
   cd /nvimbuild || exit
-  git checkout stable && make -j4 && make install
+  git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo && make install
 
 }
 
